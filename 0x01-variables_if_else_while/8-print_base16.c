c6deb6318e02cd409c26c6e1e4f63f77f37a23ed
Teboho <teboho.mofokeng@hotmail.com>
2022-09-12 05:22:57 -0700

    script push

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,16 +4,16 @@
  */
 #include <stdio.h>
 /**
- *main - Prints all the numers of base 16 lower.
- *
- * Return: Always 0.
- */
+*main - Prints all the numers of base 16 lower.
+*
+* Return: Always 0.
+*/
 int main(void)
 int num;
 char letter;
-for(num = 0; num < 10; num++)
+for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
-for(letter = 'a'; letter <= 'f'; letter++)
+for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
