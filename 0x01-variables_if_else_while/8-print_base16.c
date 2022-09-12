9a4c41183a13702cf20a5236523b550c568b5d3c
Teboho <teboho.mofokeng@hotmail.com>
2022-09-12 05:18:59 -0700

    script push

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+/*
+ * File: 8-print_base16.c
+ *Auth: Teboho Mofokeng
+ */
+#include <stdio.h>
+/**
+ *main - Prints all the numers of base 16 lower.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+int num;
+char letter;
+for(num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for(letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
