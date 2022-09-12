d212fda824372fc43a0bf154fa5821783379ad27
Teboho <teboho.mofokeng@hotmail.com>
2022-09-12 05:24:31 -0700

    script push

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,6 +9,7 @@
 * Return: Always 0.
 */
 int main(void)
+{
 int num;
 char letter;
 for (num = 0; num < 10; num++)
@@ -17,3 +18,4 @@ for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
+}
