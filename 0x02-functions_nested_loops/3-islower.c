6aaabd1480bc0f032f4f8e955f137926920462c9
Teboho <teboho.mofokeng@hotmail.com>
2022-09-13 23:04:35 -0700

    used commit Script for study purposes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,10 @@
 /**
  * _islower - Entry Point
  * Description: Check lowercase c
- *
+ * Return: Always 0 (Success)
  */
 
 int _islower(int c)
 {
-return(c >= 97 && c <= 122);
+return (c >= 97 && c <= 122);
 }
