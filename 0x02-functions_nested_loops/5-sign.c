849b637999e4c3b79dee6534ae27d0bd231a6e4f
Teboho <teboho.mofokeng@hotmail.com>
2022-09-14 00:13:50 -0700

    used commit Script for study purposes

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign: Prints the sign of a number
+ * @n: check value
+ * Return: 1 and print + if n greater than 0
+ * 0 and print 0 if n is equal to 0
+ * -1 and print - if n is less than 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}else if (n == 0)
+{
+_putchar(48);
+return (0);
+}else if (n < 0)
+{
+_putchar(45);
+return (-1)
+}
+}
