a4c86d2774846bd67b4a9cb9293f8f2bf8abc9f3
Teboho <teboho.mofokeng@hotmail.com>
2022-09-27 01:26:23 -0700

    auto commit for study purposes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,11 +8,11 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-int i = 0, j;
+int i = 0, j = 0;
 int matches = 0;
 while (s[i] != '\0')
 {
-for ((j = 0; accept[j] != '\0'; j++))
+for (j == 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
