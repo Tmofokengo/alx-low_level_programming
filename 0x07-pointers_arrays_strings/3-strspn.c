3614a926ccf10746e81c92049f71d24ae5cc7e90
Teboho <teboho.mofokeng@hotmail.com>
2022-09-27 01:47:19 -0700

    auto commit for study purposes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 unsigned int i, j, a_len = 0, len = 0;
 while (accept[a_len] != '\0')
 a_len++;
-for (i = 0; s[i] != '\0'; i++)
+for ((i = 0); s[i] != '\0'; i++)
 {
 for (j = 0; j < a_len; j++)
 {
