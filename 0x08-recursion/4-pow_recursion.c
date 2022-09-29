507b98cb89339881b0638754730ef20d42156fe6
Teboho <teboho.mofokeng@hotmail.com>
2022-09-29 01:42:21 -0700

    auto commit for study purposes

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the value of x raised to power y
+ *@x: input
+ *@y: power of
+ *Return: -1 if y lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+  if (y == 0)
+    {
+      return (
+    }
+}
