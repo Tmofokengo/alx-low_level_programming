dab2956a7be2fe3a604f4fd302b59089ffcf3141
Teboho <teboho.mofokeng@hotmail.com>
2022-09-30 01:38:11 -0700

    git push script

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints all arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+_putchar("%s\n", argv[i]);
+return (0);
+}
