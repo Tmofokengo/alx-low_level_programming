c8ecf496c2a50267bb2cb539aab9968df03d01be
Teboho <teboho.mofokeng@hotmail.com>
2022-09-30 01:31:13 -0700

    git push script

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed to it
+ *@argc: The number of arguments supplied to the program
+ *@argv: An array of pointers to the arguments
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+_putchar("%d\n", argc - 1);
+return (0);
+}
