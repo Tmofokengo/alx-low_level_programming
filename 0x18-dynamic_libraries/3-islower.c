8e5cc669eded9a5c01e49ab06a45c639fb9feaa3
Teboho <teboho.mofokeng@hotmail.com>
2022-12-13 01:17:47 -0800

    task 1

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - Entry Point
+ * Description: Check lowercase c
+ * @c: check c
+* Return: 1 if c is lowercase
+*/
+
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
